2692be4a8515676b01a8d04a73642887254d3bf7
Aedan Pettit <apettit20@wooster.edu>
2018-11-10 15:44:17 -0500

    Added comments

--- game.py
+++ game.py
@@ -1,8 +1,15 @@
 import random, os
 from scenes import *
 
+# For all functions containing game or player,
+# game and player are the global variables for the game and player that are
+# passed around to every function
+
 scene_options = ["cave", "river", "lake", "woods", "clearing"]
 
+# Class object for a game
+# Contains the current coordinate location, a list of winning coordinates,
+# the current time, and the current 'scene',
 class game:
     def __init__(self):
         self.coordinates = [0,0]
@@ -18,13 +25,18 @@ class game:
                 new_y = random.randint(-100,100)
             self.winning_coordinates.append([new_x,new_y])
 
-
+# Used to handle case when user asks for help, displays instructions located in
+# help.txt
 def handle_help():
     os.system("cat help.txt")
 
+# Used to handle case when user decides to quit, gracefully exits program
 def handle_quit():
     sys.exit(0)
 
+# Used to increment time in the game
+# game and player are the global game and player variables
+# length is the amount of time to increase by
 def time_up(game,length,player):
     game.time["hour"] += length
     if game.time["hour"] >= 24:
@@ -33,12 +45,21 @@ def time_up(game,length,player):
     player.health["energy"] -= 2 * length
     player.health["hunger"] += 2 * length
 
+# Asks user to enter an amount
+# Type is type of input they are giving, e.g. time
+# Game is the global game object, and player is the global player object
+# Does not exit gracefully if user does not enter a number
 def ask_amount(type, game, player):
     if type == "time":
         response = int(input("How many hours would you like to do this for? "))
         time_up(game,response,player)
     return response
 
+# Used to handle player's turns
+# Displays a new scene if the player has moved since the last turn
+# Asks the user what to do next
+# game and player are global variables that are passed around
+# new_scene is a boolean that dictates whether or not the user has moved
 def handle_turn(game, player, new_scene):
     for lst in game.winning_coordinates:
         if game.coordinates[0] == lst[0] and game.coordinates[1] == lst[1] or game.time["day"] >= 365:
@@ -66,6 +87,8 @@ def handle_turn(game, player, new_scene):
     answer = answer.lower()
     handle_answer(answer,player,game)
 
+# Handles user's response when asked what to do next
+# Answer is the next action
 def handle_answer(answer,player,game):
     if answer == "scavenge":
         time = ask_amount("time", game, player)
@@ -109,6 +132,7 @@ def handle_answer(answer,player,game):
         answer = answer.lower()
         handle_answer(answer, player, game)
 
+# Handles case where user has won the game
 def handle_win(player):
     x = random.randint(0,3)
     if x == 0:
@@ -134,6 +158,7 @@ def handle_win(player):
               " home!\nCongratulations", player, "you have won the game!")
     return 0
 
+# Handles cases where user has lost the game
 def handle_lose(player):
     if player.health["energy"] <= 0:
         print("You are too exhausted to go on... Sorry,", player, "you have lost...")
